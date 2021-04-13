34c5a157c5fe4f5636a8d92c8aeb2376cd096f1c
librits <librits19@gmail.com>
2021-04-13 17:22:19 +0800

    init

---
+++ data.js
@@ -0,0 +1,710 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-door",
+      "name": "door",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -0.5471925900377457,
+        "pitch": 0.1170591761428259,
+        "fov": 1.3731268825307628
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.30024753036226315,
+          "pitch": 0.26069042808268605,
+          "rotation": 0,
+          "target": "1-uno"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-uno",
+      "name": "uno",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -0.7506584405176859,
+        "pitch": 0.24867416071402637,
+        "fov": 1.3731268825307628
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.17764779109878504,
+          "pitch": 0.3220509884459588,
+          "rotation": 0,
+          "target": "3-thing"
+        },
+        {
+          "yaw": 0.6526962539012171,
+          "pitch": 0.30042599423140715,
+          "rotation": 0,
+          "target": "2-dos"
+        },
+        {
+          "yaw": -1.0975366741352683,
+          "pitch": 0.3610575404510037,
+          "rotation": 0,
+          "target": "11-tv"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.3440914431264384,
+          "pitch": -0.21014818039058092,
+          "title": "ponds among ponds",
+          "text": "<div>an exhibition of the threshold behavior &amp; nested life</div><br><div>Reassembling The Natural</div><div>Agency</div><div>John Feldman With Lynn Margulis</div><div>Anne Duk Hee Jordan With Pauline Doutreluingne</div><div>Chenyu MAO</div><div>Maximilian Prüfer</div><div>Zaanheh Project</div><div><br></div><div>Select artworks and projects in the exhibition have been generously supported by: in part, the Consulate General of the Federal Republic of Germany and the Flanders Ministry of Culture, Youth, and Media; and, in kind, the Taipei Fine Arts Museum.</div>"
+        },
+        {
+          "yaw": -1.5089638024101593,
+          "pitch": -0.11334283631467201,
+          "title": "Mao Chenyu",
+          "text": "<div>I Have What? Chinese Peasants War: The Rhetoric to Justice, 2013&nbsp;<div><br><div>Video (single-channel, color, sound); 103’\nCourtesy of the artist<br></div><div><div></div></div></div></div>"
+        },
+        {
+          "yaw": 1.377616628723846,
+          "pitch": 0.20114059588336453,
+          "title": "Maximilian Prüfer",
+          "text": "<div>A Gift From Him, 2018<br></div><div><br></div>Video (single-channel, color, sound); 11’31”\nCourtesy of the artist"
+        }
+      ]
+    },
+    {
+      "id": "2-dos",
+      "name": "dos",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -0.5521889204354622,
+        "pitch": 0.10405193823650549,
+        "fov": 1.4256478217424775
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7023040695815759,
+          "pitch": 0.4154588804465362,
+          "rotation": 0.7853981633974483,
+          "target": "4-mesa"
+        },
+        {
+          "yaw": -0.15718200470278632,
+          "pitch": 0.3614364884354426,
+          "rotation": 6.283185307179586,
+          "target": "3-thing"
+        },
+        {
+          "yaw": -0.9490207804814883,
+          "pitch": 0.20126701613337517,
+          "rotation": 6.283185307179586,
+          "target": "1-uno"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-thing",
+      "name": "thing",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -0.7777648189369337,
+        "pitch": 0.5259407777447329,
+        "fov": 1.3731268825307628
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1726228969288002,
+          "pitch": 0.40393027716680585,
+          "rotation": 0,
+          "target": "4-mesa"
+        },
+        {
+          "yaw": 0.23914168555715598,
+          "pitch": 0.3139084308402307,
+          "rotation": 0.7853981633974483,
+          "target": "6-cama"
+        },
+        {
+          "yaw": -1.6532841906513447,
+          "pitch": 0.22600592411553322,
+          "rotation": 6.283185307179586,
+          "target": "11-tv"
+        },
+        {
+          "yaw": -1.0297586964464784,
+          "pitch": 0.203470926579703,
+          "rotation": 6.283185307179586,
+          "target": "10-ventana"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1471424344032854,
+          "pitch": 0.38520810339129063,
+          "title": "Agency",
+          "text": "<div>Assembly (ponds among ponds), 1992 -<br></div><div><br></div><div>Mixed Media</div>Courtesy of the List of Boundary Things\nCommissioned by the ICA at NYU Shanghai"
+        },
+        {
+          "yaw": -3.1053801719654945,
+          "pitch": -0.23506360439029095,
+          "title": "Maximilian Prüfer",
+          "text": "<div>A Gift From Him, 2018<br></div><div><br></div>Video (single-channel, color, sound); 11’31”\nCourtesy of the artist"
+        }
+      ]
+    },
+    {
+      "id": "4-mesa",
+      "name": "mesa",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -0.44917765361187634,
+        "pitch": 0.3239721900939134,
+        "fov": 1.4256478217424775
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8665040286743633,
+          "pitch": 0.47250831675479077,
+          "rotation": 0,
+          "target": "3-thing"
+        },
+        {
+          "yaw": -1.722202081729023,
+          "pitch": 0.355427924112071,
+          "rotation": 6.283185307179586,
+          "target": "5-corner"
+        },
+        {
+          "yaw": -2.3092725594258603,
+          "pitch": 0.3152277831847812,
+          "rotation": 0.7853981633974483,
+          "target": "6-cama"
+        },
+        {
+          "yaw": 1.1511731804213383,
+          "pitch": 0.434318083584726,
+          "rotation": 0,
+          "target": "2-dos"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.043766863113028,
+          "pitch": 0.019660148262618193,
+          "title": "Reassembling Symbiosis",
+          "text": "<div>Texts, publications, video\nDesign by Reassembling the Natural with Wolfgang Hückel\nCommissioned by the ICA at NYU Shanghai"
+        },
+        {
+          "yaw": 0.04797383533406929,
+          "pitch": 0.16590364554362935,
+          "title": "John Feldman (dir.)",
+          "text": "<div>Symbiotic Earth, 2018<br></div><div><br></div>Video (single channel, color, sound); 147’\nTranslation and subtitles provided by Taipei Fine Arts Museum"
+        }
+      ]
+    },
+    {
+      "id": "5-corner",
+      "name": "corner",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -0.40269505832378094,
+        "pitch": 0.09867871990201493,
+        "fov": 1.4256478217424775
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.658205180257033,
+          "pitch": 0.28627801025675126,
+          "rotation": 4.71238898038469,
+          "target": "4-mesa"
+        },
+        {
+          "yaw": -0.9835543184297713,
+          "pitch": 0.33523872283323186,
+          "rotation": 7.0685834705770345,
+          "target": "6-cama"
+        },
+        {
+          "yaw": -1.9073319986241017,
+          "pitch": 0.23462279466351177,
+          "rotation": 0,
+          "target": "3-thing"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6009334834312465,
+          "pitch": -0.09801789538223282,
+          "title": "Anne Duk Hee Jordan with Pauline Doutreluingne",
+          "text": "<div>Diasporae, 2021<br></div><div><br></div>Mixed media installation (wedding bed, wood panels, digital prints,\nfabric, fans, mace); 241 × 212 × 165 cm\nVideo (color, sound); 21’30’’\nCourtesy of the artists\nVideo edit: Simone Serlenga\nCommissioned by the ICA at NYU Shanghai"
+        }
+      ]
+    },
+    {
+      "id": "6-cama",
+      "name": "cama",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2265546655669617,
+          "pitch": -0.03719160142694555,
+          "rotation": 4.71238898038469,
+          "target": "5-corner"
+        },
+        {
+          "yaw": -1.494391073705625,
+          "pitch": -0.0827111608517761,
+          "rotation": 1.5707963267948966,
+          "target": "7-esq"
+        },
+        {
+          "yaw": -2.857026475630466,
+          "pitch": 0.4934219076540405,
+          "rotation": 12.566370614359176,
+          "target": "8-tres"
+        },
+        {
+          "yaw": 1.0956007286508456,
+          "pitch": 0.42964423484810155,
+          "rotation": 0,
+          "target": "4-mesa"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.9982134898200421,
+          "pitch": -0.2091030367029667,
+          "title": "Anne Duk Hee Jordan with Pauline Doutreluingne",
+          "text": "<div>Diasporae, 2021<br></div><div><br></div>Mixed media installation (wedding bed, wood panels, digital prints,\nfabric, fans, mace); 241 × 212 × 165 cm\nVideo (color, sound); 21’30’’\nCourtesy of the artists\nVideo edit: Simone Serlenga\nCommissioned by the ICA at NYU Shanghai"
+        }
+      ]
+    },
+    {
+      "id": "7-esq",
+      "name": "esq",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -0.26084738764228454,
+        "pitch": 0.04154920934120554,
+        "fov": 1.4256478217424775
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5361705485779105,
+          "pitch": 0.6984320913790221,
+          "rotation": 0,
+          "target": "8-tres"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-tres",
+      "name": "tres",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.4256478217424775
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6826563279776448,
+          "pitch": 0.6761190935793167,
+          "rotation": 0,
+          "target": "9-centro"
+        },
+        {
+          "yaw": 0.6361208678158583,
+          "pitch": 0.7060409104984835,
+          "rotation": 5.497787143782138,
+          "target": "6-cama"
+        },
+        {
+          "yaw": 1.9528255595899902,
+          "pitch": 0.3286466207033314,
+          "rotation": 0,
+          "target": "3-thing"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.07194489724773234,
+          "pitch": -0.26431208173466203,
+          "title": "Anne Duk Hee Jordan with Pauline Doutreluingne",
+          "text": "<div>Diasporae, 2021<br></div><div><br></div>Mixed media installation (wedding bed, wood panels, digital prints,\nfabric, fans, mace); 241 × 212 × 165 cm\nVideo (color, sound); 21’30’’\nCourtesy of the artists\nVideo edit: Simone Serlenga\nCommissioned by the ICA at NYU Shanghai"
+        }
+      ]
+    },
+    {
+      "id": "9-centro",
+      "name": "centro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -0.5121748023125257,
+        "pitch": 0.16489733457311218,
+        "fov": 1.4256478217424775
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4985360134488346,
+          "pitch": 0.6700158531818108,
+          "rotation": 0,
+          "target": "8-tres"
+        },
+        {
+          "yaw": -0.39849302637470885,
+          "pitch": 0.507964812785417,
+          "rotation": 0,
+          "target": "3-thing"
+        },
+        {
+          "yaw": 1.0956012023750628,
+          "pitch": 0.4765503133236102,
+          "rotation": 0,
+          "target": "10-ventana"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.2871851724248806,
+          "pitch": -0.15179885334620913,
+          "title": "Maximilian Prüfer",
+          "text": "<div>A Gift From Him, 2018<br></div><div><br></div>Video (single-channel, color, sound); 11’31”\nCourtesy of the artist"
+        },
+        {
+          "yaw": 1.3442595809384255,
+          "pitch": -0.0948279840745272,
+          "title": "Monika Lin, Zaanheh Project",
+          "text": "<div>Neighborhoods, 2021&nbsp;</div><div><br></div><div>Walks; digitally printed maps\nCommissioned by the ICA at NYU Shanghai</div>"
+        },
+        {
+          "yaw": -2.0090676548093516,
+          "pitch": -0.21754943743471244,
+          "title": "Anne Duk Hee Jordan with Pauline Doutreluingne",
+          "text": "<div>Diasporae, 2021<br></div><div><br></div></div>Mixed media installation (wedding bed, wood panels, digital prints,\nfabric, fans, mace); 241 × 212 × 165 cm\nVideo (color, sound); 21’30’’\nCourtesy of the artists\nVideo edit: Simone Serlenga\nCommissioned by the ICA at NYU Shanghai"
+        }
+      ]
+    },
+    {
+      "id": "10-ventana",
+      "name": "ventana",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": 0.6256186465676237,
+        "pitch": 0.26494542972040414,
+        "fov": 1.4256478217424775
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6141811232851717,
+          "pitch": 0.39717599776720114,
+          "rotation": 0,
+          "target": "9-centro"
+        },
+        {
+          "yaw": -2.9064540793611435,
+          "pitch": 0.5346189187926562,
+          "rotation": 0,
+          "target": "11-tv"
+        },
+        {
+          "yaw": 2.4118005674275276,
+          "pitch": 0.37433868574760787,
+          "rotation": 0,
+          "target": "3-thing"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.1323648631424525,
+          "pitch": -0.06533210856313687,
+          "title": "Monika Lin, Zaanheh Project",
+          "text": "<div>Neighborhoods, 2021&nbsp;</div><div><br></div><div>Walks; digitally printed maps\nCommissioned by the ICA at NYU Shanghai</div>"
+        }
+      ]
+    },
+    {
+      "id": "11-tv",
+      "name": "tv",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.06263631892939614,
+          "pitch": 0.49527989088202773,
+          "rotation": 0,
+          "target": "10-ventana"
+        },
+        {
+          "yaw": 0.8462537329505118,
+          "pitch": 0.3907783318578204,
+          "rotation": 0,
+          "target": "9-centro"
+        },
+        {
+          "yaw": -2.984169827402244,
+          "pitch": 0.517583634062742,
+          "rotation": 0,
+          "target": "1-uno"
+        },
+        {
+          "yaw": 1.9202518880764767,
+          "pitch": 0.45821414397401483,
+          "rotation": 0,
+          "target": "3-thing"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5820244269680579,
+          "pitch": -0.12904062291577034,
+          "title": "Mao Chenyu",
+          "text": "<div>I Have What? Chinese Peasants War: The Rhetoric to Justice, 2013&nbsp;<div><br><div>Video (single-channel, color, sound); 103’ Courtesy of the artist</div></div></div>"
+        }
+      ]
+    }
+  ],
+  "name": "ICA Spring 2021: Ponds among ponds",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
